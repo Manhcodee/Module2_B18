ffd24bcf4aa757ef47bd3230e354c8b8f2123bbf
Manhcodee <phammanh2952@gmail.com>
2024-11-27 14:30:07 +0700

    NumberGererator

---
+++ B18_BT1/src/NumberGenerator.java
@@ -0,0 +1,30 @@
+public class NumberGenerator implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i + ", HashCode: " + this.hashCode());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+    }
+}
+
+class ThreadExample {
+    public static void main(String[] args) {
+
+        NumberGenerator generator1 = new NumberGenerator();
+        NumberGenerator generator2 = new NumberGenerator();
+
+        Thread thread1 = new Thread(generator1);
+        Thread thread2 = new Thread(generator2);
+
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.setPriority(Thread.MIN_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+    }
+}
